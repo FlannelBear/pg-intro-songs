6806e750999f1203bc0baa0abd0c92369f743892
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-07 11:54:11 -0500

    working post and put

--- server/modules/routes/song-router.js
+++ server/modules/routes/song-router.js
@@ -18,19 +18,39 @@ router.get('/', (req, res)=>{
 
 router.post('/', (req, res)=> {
    console.log('Handling POST for /song', req.body);
-   res.sendStatus(201);
+   const queryText = `INSERT INTO songs (artist, track, published, rank)
+                        Values
+                        ($1, $2, $3, $4);`;
+   pool.query(queryText, [req.body.artist, req.body.track, req.body.published, req.body.rank])
+   .then((result)=>{
+      console.log('Finished POST for /song', req.body);
+      res.sendStatus(200);
+   })
+   .catch((error)=> {
+      console.log('Error handling POST for /song', error);
+      res.sendStatus(500);
+   });
 });
 
-router.put('/', (req, res)=> {
-   console.log('Handling PUT for /song');
-   res.sendStatus(200);
+router.put('/:id', (req, res)=> {
+   console.log('Handling PUT for /song', req.body);
+   const id = req.params.id;
+   const queryText = 'UPDATE songs SET artist = $2, track = $3, published = $4, rank = $5 WHERE id = $1;';
+   console.log(queryText);
+   pool.query(queryText, [id, req.body.artist, req.body.track, req.body.published, req.body.rank]).then((result)=> {
+      console.log('Finished PUT for /song: ', result);
+      res.sendStatus(200);
+   }).catch((error)=>{
+      console.log('Error handling PUT for /song: ', error);
+      res.sendStatus(500);
+   });
 });
 
 router.delete('/:id', (req, res)=> {
    console.log('Handling DELETE for /song');
    const id = req.params.id;
-   const queryText = 'DELETE FROM songs WHERE id = $1';
-   pool.query(queryText, [1]).then((result)=> {
+   const queryText = `DELETE FROM songs WHERE id = $1`;
+   pool.query(queryText, [id]).then((result)=> {
       console.log('Deleted from /song', id);
       res.sendStatus(200);
    }).catch((error)=> {
