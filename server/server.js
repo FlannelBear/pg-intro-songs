a200cd0d8ee230696fa7e54db2ec8349dd21c441
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-07 09:29:49 -0500

    second commit

--- server/server.js
+++ server/server.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-// app.use(bodyParser.json()); // Maybe?
+app.use(bodyParser.json()); // Maybe?
 
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, ()=> {
+   console.log("Server up on port: ", PORT);
+})
